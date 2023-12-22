18f286308a35840add08dcb0a107d8027713396c
takeoverz <154090253+takeoverzzz@users.noreply.github.com>
2023-12-22 17:17:24 -0500

    Update About.cs

--- vMenu/menus/About.cs
+++ vMenu/menus/About.cs
@@ -10,14 +10,14 @@ namespace vMenuClient.menus
         private void CreateMenu()
         {
             // Create the menu.
-            menu = new Menu("vMenu", "About PF-vMenu");
+            menu = new Menu("vMenu", "About TK-vMenu");
 
             // Create menu items.
-            var version = new MenuItem("PF-vMenu Version", $"This server is using PF-vMenu ~b~~h~{MainMenu.Version}~h~~s~. \n\nDownload PF-vMenu: ~b~~h~github.com/ProjectFairnessLabs/PF-vMenu~h~~s~")
+            var version = new MenuItem("TK-vMenu Version", $"This server is using TK-vMenu ~b~~h~{MainMenu.Version}~h~~s~. \n\nDownload TK-vMenu: ~b~~h~github.com/takeoverzzz/TK-vMenu~h~~s~")
             {
                 Label = $"~h~{MainMenu.Version}~h~"
             };
-            var credits = new MenuItem("About vMenu / Credits", "vMenu is made by ~b~Vespura~s~. Extra modifications are done by members of ~b~Project Fairness Labs~b~.");
+            var credits = new MenuItem("About vMenu / Credits", "vMenu is made by ~b~Vespura~s~. Extra modifications are done by members of TK Labs");
 
             var serverInfoMessage = vMenuShared.ConfigManager.GetSettingsString(vMenuShared.ConfigManager.Setting.vmenu_server_info_message);
             if (!string.IsNullOrEmpty(serverInfoMessage))
